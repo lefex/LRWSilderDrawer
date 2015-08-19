b8cddea698c7f7ebd26aa55db96cfdff8f2ede17
王素燕 <wsyxyxs@126.com>
2015-08-19 08:05:21 +0800

    first new project LRWSilderDrawer

---
+++ LRWSilderDrawer/LRWSilderDrawerTests/LRWSilderDrawerTests.swift
@@ -0,0 +1,36 @@
+//
+//  LRWSilderDrawerTests.swift
+//  LRWSilderDrawerTests
+//
+//  Created by wsy on 15/8/19.
+//  Copyright (c) 2015年 WSY. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class LRWSilderDrawerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
